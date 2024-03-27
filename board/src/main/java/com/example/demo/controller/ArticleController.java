05192027cd446cf4e1a40703e838869154cd9272
hey5nee123 <gpdnjsl123@naver.com>
2024-03-28 05:23:51 +0900

    아무래도 entity는 없애야겠지...

--- board/src/main/java/com/example/demo/controller/ArticleController.java
+++ board/src/main/java/com/example/demo/controller/ArticleController.java
@@ -5,17 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dto.ArticleForm;
+import com.example.demo.entity.Article;
 
 @Controller
 public class ArticleController {
-	@GetMapping("/new")
-	public String newArticleForm() {
-		return "articles/new";
+	Article article = form.toEntity();
+
+	@GetMapping("/articles/neww")
+	public void neww() {
+
 	}
 
 	@PostMapping("/articles/create")
-	public String createArticleForm(ArticleForm form) {
+	public String createArticle(ArticleForm form) {
 		System.out.println(form.toString());
 		return "";
 	}
+
 }
